136af501898878cfee929df006ef5af18052f030
suhailvs <suhailvs@gmail.com>
2021-10-15 16:39:51 +0530

    celerytask added to admin

--- schools/admin.py
+++ schools/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import School
+from .models import School,CeleryTasks
 
 admin.site.register(School)
+admin.site.register(CeleryTasks)
