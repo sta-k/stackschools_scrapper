a06664c3161dd187f7f3a4d57d2f034e48e17db3
suhailvs <suhailvs@gmail.com>
2021-10-19 06:35:40 +0530

    skip existing schools

--- schools/tasks.py
+++ schools/tasks.py
@@ -1,6 +1,7 @@
 from celery import shared_task
 import requests
 from django.utils import timezone
+from django.db.models import Max
 from schools.models import School, CeleryTasks
 
 
@@ -8,11 +9,11 @@ from schools.models import School, CeleryTasks
 def task_scrap_schools(limit=5000000):
     tasklog = CeleryTasks.objects.create(name="task_scrap_schools", started= timezone.now())
     get_text = lambda code: requests.post('https://src.udiseplus.gov.in/searchSchool/getSchoolDetail', data={'schoolIdforDashSearch': code})
-
-    for sid in range(1000001,1000001+limit):
-        if School.objects.filter(code = sid): 
-            # if school data already exists then skip
-            continue
+    start = School.objects.annotate(c=Max('code'))['c'] or 1000001
+    for sid in range(start,start+limit):
+        # if School.objects.filter(code = sid): 
+        #     # if school data already exists then skip
+        #     continue
 
         r = get_text(sid)
         if r.text:
