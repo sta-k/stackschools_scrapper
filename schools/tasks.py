3162e78d860704406486299ade8fb54dc72e8118
suhailvs <suhailvs@gmail.com>
2021-10-27 12:22:19 +0530

    fixed #1: Some times celery breaks halfway with error

--- schools/tasks.py
+++ schools/tasks.py
@@ -1,5 +1,6 @@
 from celery import shared_task
 import requests
+from requests.exceptions import ConnectionError
 from django.utils import timezone
 from django.db.models import Max
 from schools.models import School, CeleryTasks
@@ -7,6 +8,7 @@ from bs4 import BeautifulSoup
 
 @shared_task
 def task_scrap_schools(limit=5000000):
+    errors={}
     tasklog = CeleryTasks.objects.create(name="task_scrap_schools", started= timezone.now())
     get_text = lambda code: requests.post('https://src.udiseplus.gov.in/searchSchool/getSchoolDetail', data={'schoolIdforDashSearch': code})
     start = School.objects.aggregate(c=Max('code'))['c'] or 1000000
@@ -14,8 +16,19 @@ def task_scrap_schools(limit=5000000):
         # if School.objects.filter(code = sid): 
         #     # if school data already exists then skip
         #     continue
+        try:
+            r = get_text(sid)
+        except ConnectionError:
+            if sid in errors:
+                # if tried this once
+                tasklog.name=f'error: {sid}'
+                break
+                
+            else:
+                r = get_text(sid)
+                errors[sid]='error'
 
-        r = get_text(sid)
+        
         if r.text:
             # if some text in response create school
             soup = BeautifulSoup(r.text, 'html.parser')
