b9518442e79b031567ecc18d067e314618a80026
suhailvs <suhailvs@gmail.com>
2021-11-10 03:57:12 +0530

    [wip] display school like https://schools.org.in/palakkad/32060200110/asmmhss-alathur.html

--- schools/views.py
+++ schools/views.py
@@ -18,8 +18,8 @@ class HomeView(View):
 
     def post(self, request):
         messages.success(request, 'Task Added Successfully!')
-        task_scrap_schools.delay(int(request.POST['number_of_items'])) # first 21 items
         print(int(request.POST['number_of_items']))
+        task_scrap_schools.delay(int(request.POST['number_of_items'])) # first 21 items
         return redirect('schools:home')
         
 
@@ -28,8 +28,61 @@ def show_school(request,code):
     if not school.html.startswith('<ul>'):
         return render(request,'schools/show.html',{'school':school})
     
-    resp = {
+    context = {
         'code': school.code,
-        'html':school.html
+        'html':school.html,
+        'school_profile':{
+            'school_name':'Hamidia Public School Rampore',
+            'udise_code':'01020503406',
+            'state':'Jammu & Kashmir',
+            'district':'BARAMULA',
+            'block':'dangerpora',
+            'cluster':'c1_bms_watlab',
+            'village':'rampora',
+            'pincode':'193201',
+            'school_category':'1-primary',
+            'school_type':'3-co-educational',
+            'class_from':'1',
+            'class_to':'5',
+            'state_management':'5-private_unaided_(recognized)',
+            'national_management':'5-private_unaided_(recognized)',
+            'status':'0-operational',
+            'location':'1-rural',
+        },
+        'basic_details': {
+            'aff_board_sec':'',
+            'aff_board_hsec':'',
+            'year_of_establishment':'2015',
+            'pre_primary':'1-Yes'
+        },
+        'facilities': {
+            'building_status':'1-private',
+            'boundary_wall':'6-others',
+            'no_of_boys_toilets':'1',
+            'no_of_girls_toilets':'0',
+            'no_of_cwsn_toilets':'0',
+            'drinking_water_availability':'yes',
+            'hand_wash_facility':'yes',
+            'functional_generator':'0',
+            'library':'1-yes',
+            'reading_corner':'1-yes',
+            'book_bank':'1-yes',
+            'functional_laptop':'1',
+            'functional_desktop':'0',
+            'functional_tablet':'0',
+            'functional_scanner':'0',
+            'functional_printer':'0',
+            'functional_led':'0',
+            'functional_digiboard':'0',
+            'internet':'2-no',
+            'dth':'2-no',
+            'functional_web_cam':'0',
+        },
+        'room_details':{
+            'class_rooms':'8',
+            'other_rooms':'1'
+        },
+        'enrolment_of_the_students':['37','12','37','12','37','12','37','12','37','12','37','12','last'],
+        'total_teachers':'4'
     }
-    return render(request,'schools/show_new.html',{'school':resp})
+    return render(request,'schools/show_new.html',context)
